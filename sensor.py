9e8acac497ba52c949448aae1b4ec18acbc048e7
k-jingyang <k.jingyang@gmail.com>
2021-09-30 11:22:32 +0800

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,15 +1,20 @@
-import random
-import pandas as pd
-from fastparquet import write
 from time import sleep
-# from board import D14
-# import adafruit_dht
+import logging as log
+import os
+
+from board import D14
+import adafruit_dht
 
 import grpc
 import collector_pb2_grpc
 import collector_pb2
 
-# dht_device = adafruit_dht.DHT22(D14)
+dht_device = adafruit_dht.DHT22(D14)
+
+log.basicConfig(
+    level=os.environ.get("LOGLEVEL", "INFO"),
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%H:%S')
 
 def poll(stub : collector_pb2_grpc.CollectorStub):
     tick = 0 
@@ -20,24 +25,23 @@ def poll(stub : collector_pb2_grpc.CollectorStub):
             sleep(1)
             tick += 1
 
-            humidity =  random.random() * 76.6 # dht_device.humidity 
-            temperature = random.random() * 29.6 # dht_device.temperature 
+            humidity =   dht_device.humidity 
+            temperature = dht_device.temperature 
 
-            print("Humidity:", humidity)
-            print("Temparature:", temperature)
+            if humidity == None or temperature == None:
+                continue
 
-            data_point = [humidity, temperature]
+            log.debug("Humidity: %s", humidity)
+            log.debug("Temperature: %s", temperature)
 
             # May want to yield this instead
             reading = collector_pb2.Reading(humidity=humidity, temperature=temperature)
             stub.SendReading(reading)
 
-            data.append(data_point)
-
-        except Exception as e:
-            print(e)
+        except RuntimeError as e:
+            log.warning(e)
             continue
 
-with grpc.insecure_channel('localhost:8080') as channel:
+with grpc.insecure_channel('192.168.0.161:8080') as channel:
     stub = collector_pb2_grpc.CollectorStub(channel)
     poll(stub)
