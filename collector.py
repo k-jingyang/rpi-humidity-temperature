925d24bd71774670ad88c0dd234a7c334d77cedd
k-jingyang <k.jingyang@gmail.com>
2021-10-02 21:04:05 +0800

    Cleanup collector.py

--- collector.py
+++ collector.py
@@ -1,14 +1,23 @@
-import grpc
+import logging as log
+import os
 from concurrent import futures
+
+import grpc
 import collector_pb2_grpc
 import collector_pb2
+
 import pandas as pd
 from fastparquet import write
 
 # Should the Parquet file contain time too? Are timestamps treated specially as compared to normal data?
 
 _PERSIST_INTERVAL = 10
+_LISTENING_PORT = 8080
 
+log.basicConfig(
+    level=os.environ.get("LOGLEVEL", "INFO"),
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%H:%S')
 
 class Collector(collector_pb2_grpc.CollectorServicer):
 
@@ -20,6 +29,9 @@ class Collector(collector_pb2_grpc.CollectorServicer):
         temperature = request.temperature
         humidity = request.humidity
         self.tick+=1
+
+        log.debug("Humidity: %s, Temperature: %s", humidity, temperature)
+        self.data.append([humidity, temperature])
 
         if self.tick % _PERSIST_INTERVAL == 0:
             df = pd.DataFrame(self.data,  columns=["humidity", "temperature"])
@@ -35,9 +47,9 @@ class Collector(collector_pb2_grpc.CollectorServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     collector_pb2_grpc.add_CollectorServicer_to_server(Collector(), server)
-    server.add_insecure_port('[::]:8080')
+    server.add_insecure_port(f'[::]:{_LISTENING_PORT}')
     server.start()
-    print("Started listening..")
+    log.info("Started listening at %s..", _LISTENING_PORT)
     server.wait_for_termination()
 
 
